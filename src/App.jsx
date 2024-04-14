4e3d44108a4889aedd781d321d09760965a559e0
kafi9411 <kafi9411@gamil.com>
2024-04-14 23:23:03 +0600

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+import './App.css'
+import DaisyNav from './components/DaisyNav'
+
+function App() {
+  
+
+  return (
+    <>
+        <DaisyNav></DaisyNav>
+      <h1 className='text-5xl bg-red-500 font-extrabold text-center p-4'>Kafi</h1>
+    </>
+  )
+}
+
+export default App
